86e88c47a1d92d11c2948836b9e206a60993b351
Jen Young <jyoung488@gmail.com>
2017-08-22 08:47:09 -0700

    Add tests for Unclaimed Draft Edit and Resend endpoint

--- spec/hello_sign/api/unclaimed_draft_spec.rb
+++ spec/hello_sign/api/unclaimed_draft_spec.rb
@@ -31,5 +31,23 @@ describe HelloSign::Api::UnclaimedDraft do
         expect(@unclaimed_draft).to be_an HelloSign::Resource::UnclaimedDraft
       end
     end
+
+    context 'edit_and_resend' do
+      before do
+        stub_post('/unclaimed_draft/edit_and_resend/1', 'unclaimed_draft')
+        @unclaimed_draft = HelloSign.edit_and_resend_unclaimed_draft(
+          :signature_request_id => '1',
+          :client_id => '5e365c014bea2e9a05a9d0834f3e7ca4'
+        )
+      end
+
+      it 'should get the correct resource' do
+        expect(a_post('/unclaimed_draft/edit_and_resend/1')).to have_been_made
+      end
+
+      it 'should return an UnclaimedDraft response' do
+        expect(@unclaimed_draft).to be_a HelloSign::Resource::UnclaimedDraft
+      end
+    end
   end
 end
