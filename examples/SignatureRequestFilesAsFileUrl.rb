9213ccd74deba6961c1e9f9934dd9b54503d6315
fenghao <fenghao@dropbox.com>
2022-09-29 15:36:35 +0100

    add missing files from previous commit

---
+++ examples/SignatureRequestFilesAsFileUrl.rb
@@ -0,0 +1,20 @@
+require "hellosign-ruby-sdk"
+
+HelloSign.configure do |config|
+  # Configure HTTP basic authorization: api_key
+  config.username = "YOUR_API_KEY"
+
+  # or, configure Bearer (JWT) authorization: oauth2
+  # config.access_token = "YOUR_ACCESS_TOKEN"
+end
+
+api = HelloSign::SignatureRequestApi.new
+
+signature_request_id = "fa5c8a0b0f492d768749333ad6fcc214c111e967"
+
+begin
+  result = api.signature_request_files_as_file_url(signature_request_id)
+  p result
+rescue HelloSign::ApiError => e
+  puts "Exception when calling HelloSign API: #{e}"
+end
---
+++ examples/TemplateFilesAsFileUrl.rb
@@ -0,0 +1,20 @@
+require "hellosign-ruby-sdk"
+
+HelloSign.configure do |config|
+  # Configure HTTP basic authorization: api_key
+  config.username = "YOUR_API_KEY"
+
+  # or, configure Bearer (JWT) authorization: oauth2
+  # config.access_token = "YOUR_ACCESS_TOKEN"
+end
+
+api = HelloSign::TemplateApi.new
+
+template_id = "5de8179668f2033afac48da1868d0093bf133266"
+
+begin
+  result = api.template_files_as_file_url(template_id)
+  p result
+rescue HelloSign::ApiError => e
+  puts "Exception when calling HelloSign API: #{e}"
+end
